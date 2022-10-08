4d61a67437fded8fbf779766da47fa3a2c28c0df
AntonioNappi988 <74452384+AntonioNappi988@users.noreply.github.com>
2022-10-08 16:43:11 +0200

    added sum/division/moltiplication/subtraction functions in Calcolatrice

--- src/main/java/org/example/Calcolatrice.java
+++ src/main/java/org/example/Calcolatrice.java
@@ -1,4 +1,33 @@
 package org.example;
 
 public class Calcolatrice {
+
+    private int a,b;
+
+    public Calcolatrice(){
+        this.a=0;
+        this.b=0;
+    }
+
+
+
+    public int somma(int a,int b){
+        return a+b;
+    }
+
+    public int sottrazione(int a, int b){
+        return a-b;
+    }
+
+    public int moltiplicazione(int a, int b){
+        return a*b;
+    }
+
+    public int divisione(int a, int b){
+        return a/b;
+    }
+
+
+
+
 }
